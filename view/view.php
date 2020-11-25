0510e69602af0a8e5786a83fe6857bce52176c0f
Sifulo <contact.stephane.lo@gmail.com>
2020-11-25 15:31:54 +0100

    ''

--- view/view.php
+++ view/view.php
@@ -1,19 +1,41 @@
+<?php
+  $num_baniere = rand(1,6);
+  $fleur = isset($_GET['fleur']) ? $_GET['fleur'] : "rose";
+  /*  remarque PHP : la structure "ternaire" ci-dessus
+      est exactement équivalente au code suivant :
+    if (isset($_GET['fleur'])) {
+      $fleur = $_GET['fleur'];
+    } else {
+      $fleur = "rose";
+    }
+  */
+?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../img/icones/fleur.ico" rel="icon" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="../css/stylesDivers.css">
+    <link rel="stylesheet" type="text/css" href="../css/stylesBaniere.css">
+    <link rel="stylesheet" type="text/css" href="../css/stylesMenu.css">
+    <link rel="stylesheet" type="text/css" href="../css/stylesGalerie.css">
         <title><?php echo $pagetitle; ?></title>
     </head>
     <body>
     	<header>
-    		<nav>
-    			<p style="border: 1px solid black;text-align:left;padding-right:1em;">
-    				<a href="index.php?controller=voiture&action=readAll">Accueil</a>
-    				<a href="index.php?controller=utilisateur&action=readAll">Utilisateurs</a>
-    				<a href="index.php?controller=trajet&action=readAll">Trajets</a>
-    			</p>
-    		</nav>
-    	</header>
+        <div id="baniere">
+            <img id="1" class="img_baniere visible" alt="baniere" src="../img/baniere/baniere<?php echo $num_baniere; ?>.jpg">
+        </div>
+        <nav>
+          <ul>
+            <li><a href="index.php?fleur=rose">rose</a></li>
+            <li><a href="index.php?fleur=hortensia">hortensia</a></li>
+            <li><a href="index.php?fleur=fruitier">fruitier</a></li>
+            <li><a href="index.php?fleur=autre">autre</a></li>
+          </ul>
+        </nav>
+      </header>
 		<?php
 		// Si $controleur='voiture' et $view='list',
 		// alors $filepath="/chemin_du_site/view/voiture/list.php"
@@ -21,9 +43,8 @@
 		require $filepath;
 		?>
 		<footer>
-			<p style="border: 1px solid black;text-align:right;padding-right:1em;">
-  					Site de covoiturage de Fu Lo
-			</p>
-		</footer>
+        <p>PHP 2020</p>
+        <p>Site E-Commerce de Floralys</p>
+      </footer>
     </body>
 </html>
