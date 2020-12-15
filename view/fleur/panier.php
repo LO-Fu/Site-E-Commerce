48ee937040fe1cb0af8d683597a6f677377ae6c3
Hugo Davron <hugo.davron@etu.umontpellier.fr>
2020-12-15 22:12:11 +0100

    add cart visibility

--- view/fleur/panier.php
+++ view/fleur/panier.php
@@ -1,9 +1,52 @@
-<?php
-    var_dump($_SESSION['panier']);
-    foreach($_SESSION['panier'] as $attribute){
-        if (is_array($attribute)){
-            foreach ($attribute as $key => $value){
-                echo $value;
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Votre panier</title>
+</head>
+<body>
+
+<form method="post" action="panier.php">
+    <table style="width: 400px">
+        <tr>
+            <td colspan="4">Votre panier</td>
+        </tr>
+        <tr>
+            <td>Libellé</td>
+            <td>Quantité</td>
+            <td>Prix Unitaire</td>
+            <td>Action</td>
+        </tr>
+
+
+        <?php
+            $nbArticles=count($_SESSION['panier']['id']);
+            if ($nbArticles <= 0)
+                echo "<tr><td>Votre panier est vide </ td></tr>";
+            else
+            {
+                for ($i=0 ;$i < $nbArticles ; $i++)
+                {
+                    echo "<tr>";
+                    echo "<td>".htmlspecialchars($_SESSION['panier']['id'][$i])."</ td>";
+                    echo "<td><input type=\"text\" size=\"4\" name=\"q[]\" value=\"".htmlspecialchars($_SESSION['panier']['qte'][$i])."\"/></td>";
+                    echo "<td>".htmlspecialchars($_SESSION['panier']['prix'][$i])."</td>";
+                    echo "<td><a href=\"".htmlspecialchars("panier.php?action=suppression&i=".rawurlencode($_SESSION['panier']['id'][$i]))."\">XX</a></td>";
+                    echo "</tr>";
+                }
+
+                echo "<tr><td colspan=\"2\"> </td>";
+                echo "<td colspan=\"2\">";
+                echo "Total : ".MontantGlobal();
+                echo "</td></tr>";
+
+                echo "<tr><td colspan=\"4\">";
+                echo "<input type=\"submit\" value=\"Rafraichir\"/>";
+                echo "<input type=\"hidden\" name=\"action\" value=\"refresh\"/>";
+
+                echo "</td></tr>";
             }
-        }
-    }
+        ?>
+    </table>
+</form>
+</body>
+</html>
