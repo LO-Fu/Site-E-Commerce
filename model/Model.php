a61c1326d1662e9a04f812c21028ee88b535a2bb
Hugo Davron <hugo.davron@etu.umontpellier.fr>
2020-12-15 06:07:33 +0100

    generic update function

--- model/Model.php
+++ model/Model.php
@@ -73,7 +73,7 @@ class Model{
             $sql = "UPDATE :object SET ";
             $pkey = static::$primary;
             $infos = get_object_vars($data);
-            var_dump(get_object_vars($data));
+            print_r(get_object_vars($data));
             $values = array(":object" => static::$object);
             foreach ($infos as $clef=> $value) {
                 echo $clef;
