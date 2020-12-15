6b9c66ecb184d2bf26fe988aa6915f773096f537
Hugo Davron <hugo.davron@etu.umontpellier.fr>
2020-12-15 02:27:43 +0100

    debug

--- view/fleur/updated.php
+++ view/fleur/updated.php
@@ -1,3 +1,3 @@
 <?php
 echo "<p>La $f[0] de couleur $f[1]  a bien été modifié.</p>";
-require File::build_path(array("view","list.php"));
+require File::build_path(array("view","fleur","list.php"));
