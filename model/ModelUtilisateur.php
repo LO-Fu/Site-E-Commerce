14d8faacba541145156e6be2a7fbb7d31f57cb28
Hugo <hugo.davron@etu.umontpellier.fr>
2020-12-16 13:52:12 +0100

    debug

--- model/ModelUtilisateur.php
+++ model/ModelUtilisateur.php
@@ -50,7 +50,7 @@ class ModelUtilisateur extends Model{
         );
         $req_prep->execute($values);
         $req_prep->setFetchMode(PDO::FETCH_CLASS, 'ModelUtilisateur');
-        $u = $req_prep->fetchAll();
+        $u = $req_prep->fetch();
         $mdp = $u[0]->get('mdp');
         if ($mdp == $mot_de_passe_hache){
             return true;
